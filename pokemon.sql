9f579e2da9e29a6d6272d07584c84edb2fc41565
Benjamin de Vaublanc <benjamin.devaublanc@gmail.com>
2012-11-12 12:19:23 +0100

    Update Sql Database

--- pokemon.sql
+++ pokemon.sql
@@ -1,11 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 3.5.1
+﻿-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
 -- http://www.phpmyadmin.net
 --
--- Client: localhost
--- Généré le: Ven 12 Octobre 2012 à 14:29
--- Version du serveur: 5.5.24-log
--- Version de PHP: 5.3.13
+-- Client: 127.0.0.1
+-- Généré le: Lun 12 Novembre 2012 à 12:18
+-- Version du serveur: 5.5.27-log
+-- Version de PHP: 5.4.6
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,136 +17,65 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données: `pokemondp`
+-- Base de données: `pokemon`
 --
-CREATE DATABASE `pokemondp` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `pokemondp`;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `attaque`
+-- Structure de la table `pokemons`
 --
 
-CREATE TABLE IF NOT EXISTS `attaque` (
+CREATE TABLE IF NOT EXISTS `pokemons` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nomattaque` varchar(255) NOT NULL,
-  `puissance` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `xp` int(11) NOT NULL,
+  `currentpv` int(11) NOT NULL,
+  `attack1` varchar(255) NOT NULL,
+  `attack2` varchar(255) NOT NULL,
+  `attack3` varchar(255) NOT NULL,
+  `defense` int(11) NOT NULL,
+  `id_attack` int(11) NOT NULL,
+  `id_stat` int(11) NOT NULL,
+  `level` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=32 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- Contenu de la table `attaque`
+-- Contenu de la table `pokemons`
 --
 
-INSERT INTO `attaque` (`id`, `nomattaque`, `puissance`) VALUES
-(3, 'Charge', 10),
-(4, 'Flammeche', 15),
-(7, 'Rugissement', 0),
-(8, 'Griffe', 20),
-(9, 'Vampigraine', 0),
-(10, 'Fouet Lianes', 15),
-(11, 'Mimi-Queue', 0),
-(12, 'Ecume ', 20),
-(13, 'Pistolet A O', 40),
-(14, 'Morsure ', 60),
-(15, 'Coud''Krane ', 90),
-(16, 'Hydrocanon', 120),
-(17, 'Lance-Flamme', 50),
-(18, 'Incendie', 100),
-(21, 'Tranch''Herbe', 70),
-(22, 'Canon Graine ', 110),
-(23, 'Jet De Sable', 0),
-(24, 'Tornade', 30),
-(25, 'Vive-Attaque', 40),
-(26, 'Cyclone', 70),
-(27, 'Ouragan', 120),
-(28, 'Secretion', 0),
-(29, 'Piqure', 20),
-(30, 'Choc Mental ', 50),
-(31, 'Puissance', 0);
+INSERT INTO `pokemons` (`id`, `nom`, `type`, `img`, `xp`, `currentpv`, `attack1`, `attack2`, `attack3`, `defense`, `id_attack`, `id_stat`, `level`) VALUES
+(1, 'bulbizarre', 'plante', 'bulbizarre.png', 0, 45, '49', '', '', 49, 1, 1, 60);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `pokemon`
+-- Structure de la table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `pokemon` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nompokemon` varchar(255) NOT NULL,
-  `XP` int(11) NOT NULL,
-  `PV` int(11) NOT NULL,
-  `Attaque1` int(11) NOT NULL,
-  `Attaque2` int(11) NOT NULL,
-  `Attaque3` int(11) NOT NULL,
-  `Attaque4` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `Attaque3` (`Attaque3`),
-  KEY `Attaque4` (`Attaque4`),
-  KEY `Attaque1` (`Attaque1`),
-  KEY `Attaque2` (`Attaque2`),
-  KEY `Attaque2_2` (`Attaque2`),
-  KEY `Attaque1_2` (`Attaque1`),
-  KEY `Attaque2_3` (`Attaque2`),
-  KEY `nompokemon` (`nompokemon`),
-  KEY `nompokemon_2` (`nompokemon`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `mdp` varchar(11) NOT NULL,
+  `direction` varchar(255) NOT NULL,
+  `positiony` int(11) NOT NULL,
+  `positionx` int(11) NOT NULL,
+  `map` varchar(255) NOT NULL,
+  `id_pokemon` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- Contenu de la table `pokemon`
+-- Contenu de la table `users`
 --
 
-INSERT INTO `pokemon` (`id`, `nompokemon`, `XP`, `PV`, `Attaque1`, `Attaque2`, `Attaque3`, `Attaque4`) VALUES
-(1, 'Salamech', 0, 50, 3, 4, 7, 8),
-(5, 'Bulbizarre', 0, 50, 3, 7, 9, 10),
-(6, 'Carapuce', 0, 50, 3, 7, 11, 12),
-(7, 'Roucool', 0, 50, 3, 23, 24, 25),
-(8, 'Chenipan', 0, 50, 3, 26, 27, 28),
-(9, 'Rattata', 0, 50, 3, 11, 25, 31);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `utilisateur`
---
-
-CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Nom` varchar(255) NOT NULL,
-  `NomPokemon` varchar(255) NOT NULL,
-  `Victoire` int(11) NOT NULL,
-  `Defaite` int(11) NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `NomPokemon` (`NomPokemon`),
-  KEY `NomPokemon_2` (`NomPokemon`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
-
---
--- Contenu de la table `utilisateur`
---
-
-INSERT INTO `utilisateur` (`ID`, `Nom`, `NomPokemon`, `Victoire`, `Defaite`) VALUES
-(1, 'user', 'Salamech', 0, 0);
-
---
--- Contraintes pour les tables exportées
---
-
---
--- Contraintes pour la table `pokemon`
---
-ALTER TABLE `pokemon`
-  ADD CONSTRAINT `pokemon_ibfk_5` FOREIGN KEY (`Attaque1`) REFERENCES `attaque` (`id`),
-  ADD CONSTRAINT `pokemon_ibfk_6` FOREIGN KEY (`Attaque2`) REFERENCES `attaque` (`id`),
-  ADD CONSTRAINT `pokemon_ibfk_7` FOREIGN KEY (`Attaque3`) REFERENCES `attaque` (`id`),
-  ADD CONSTRAINT `pokemon_ibfk_8` FOREIGN KEY (`Attaque4`) REFERENCES `attaque` (`id`);
-
---
--- Contraintes pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  ADD CONSTRAINT `utilisateur_ibfk_1` FOREIGN KEY (`NomPokemon`) REFERENCES `pokemon` (`nompokemon`);
+INSERT INTO `users` (`id`, `nom`, `prenom`, `login`, `mdp`, `direction`, `positiony`, `positionx`, `map`, `id_pokemon`) VALUES
+(1, 'de Vaublanc', 'Benjamin', 'mewmew', 'abc123', 'down', 6, 5, 'city', 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
